b32fbaa3713a30e65520dccfea1d0158df7d37df
xujianxing <xujianxing@corp.netease.com>
2021-08-13 19:51:44 +0800

    leetcode 103. 二叉树的锯齿形层序遍历

---
+++ src/foroffer/leetcode/lc103_zigzagLevelOrder.java
@@ -0,0 +1,55 @@
+package foroffer.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 103. 二叉树的锯齿形层序遍历
+ * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * 返回锯齿形层序遍历如下：
+ *
+ * [
+ *   [3],
+ *   [20,9],
+ *   [15,7]
+ * ]
+ */
+public class lc103_zigzagLevelOrder {
+}
+
+class Solution103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<>();
+        if (root == null) return ans;
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        boolean isOrderLeft = true;
+        while (!nodeQueue.isEmpty()){
+            Deque<Integer> levelList = new LinkedList<>();
+            int size = nodeQueue.size();
+            // 取出一行来
+            for (int i = 0; i < size; i++) {
+                TreeNode curNode = nodeQueue.poll();  // poll方法可以返回null
+                if (isOrderLeft){
+                    levelList.addLast(curNode.val);
+                } else {
+                  levelList.addFirst(curNode.val);
+                }
+                // 把下一行加入
+                if (curNode.left != null){
+                    nodeQueue.offer(curNode.left);
+                }
+                if (curNode.right != null){
+                    nodeQueue.offer(curNode.right);
+                }
+            }
+            ans.add(new LinkedList<>(levelList));
+            isOrderLeft = !isOrderLeft;
+        }
+        return ans;
+    }
+}
