2814d8c6341033ab5ecfc8ac197ea4a7e23ec7be
xujianxing <xujianxing@corp.netease.com>
2021-08-19 11:24:24 +0800

    leetcode 236. 二叉树的最近公共祖先

---
+++ src/foroffer/leetcode/lc236_lowestCommonAncestor.java
@@ -0,0 +1,76 @@
+package foroffer.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 236. 二叉树的最近公共祖先
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大
+ * （一个节点也可以是它自己的祖先）。”
+ *
+ */
+public class lc236_lowestCommonAncestor {
+}
+
+class Solution236 {
+    /**
+     * 法1 深度遍历，遍历的时候记录有没有找到过p或q，如果找到了就一直返回找到了，然后在方法体中间一直判断概念定义条件
+     */
+    private TreeNode ans;
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+        boolean lson = dfs(root.left, p, q);
+        boolean rson = dfs(root.right, p, q);
+        // ans会赋值多次，但是最后一次赋值肯定是最近的公共祖先，因为再往后左右条件不能同时满足了
+        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
+            ans = root;
+        }
+        // 左右为true以后就一直都是true。为true以后一直向上找，直到又也为true或者本身就是父节点，
+        return lson || rson || (root.val == p.val || root.val == q.val);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        this.dfs(root, p, q);
+        return this.ans;
+    }
+
+    /**
+     * 方法二：存储父节点
+     * 用哈希表存所有节点对应的父节点，然后利用节点的父节点信息从p节点一直往上找，并记录找过的节点，
+     * 再从q开始不断往上找，直到遇到访问过的节点。
+     */
+    Map<Integer, TreeNode> parent = new HashMap<>();
+    Set<Integer> visited = new HashSet<>();
+
+    private void dfs(TreeNode root) {
+        if (root.left != null) {
+            parent.put(root.left.val, root);
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            parent.put(root.right.val, root);
+            dfs(root.right);
+        }
+    }
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root);
+        while (p != null) {
+            visited.add(p.val);
+            p = parent.get(p.val);
+        }
+        while (q != null) {
+            if (visited.contains(q.val)) {
+                return q;
+            }
+            q = parent.get(q.val);
+        }
+        return null;
+    }
+
+}
