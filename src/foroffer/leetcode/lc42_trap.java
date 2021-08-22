ca91d20f14eb040adb3e13895ff10ce6f8fe5c62
XuJianxing <498802124@qq.com>
2021-08-22 18:45:27 +0800

    leetcode 42. 接雨水

---
+++ src/foroffer/leetcode/lc42_trap.java
@@ -0,0 +1,65 @@
+package foroffer.leetcode;
+
+/**
+ * 42. 接雨水
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ */
+public class lc42_trap {
+}
+
+class Solution42 {
+    /**
+     * 双指针
+     * 仔细分析问题后得到的数学规律：当前坐标的蓄水量是这个位置的左边极大值和右边极大值中较小的值拦住的
+     */
+    public int trap(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 动态规划
+     * 对于下标 ii，下雨后水能到达的最大高度等于下标 ii 两边的最大高度的最小值
+     * 用两个数组存从左到右和从右到左的最大值数组，然后对每个位置计算水位
+     */
+    public int trap2(int[] height) {
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; ++i) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
+
+}
