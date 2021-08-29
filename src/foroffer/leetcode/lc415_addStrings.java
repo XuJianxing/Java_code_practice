72a9390f1528baebcdc85f69fac177f147d24c8b
XuJianxing <498802124@qq.com>
2021-08-29 18:54:25 +0800

    leetcode 415. 字符串相加

---
+++ src/foroffer/leetcode/lc415_addStrings.java
@@ -0,0 +1,25 @@
+package foroffer.leetcode;
+
+public class lc415_addStrings {
+    /**
+     * 415. 字符串相加
+     * 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+     */
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+        StringBuffer ans = new StringBuffer();  // 线程安全
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int result = x + y + add;
+            ans.append(result % 10);
+            add = result / 10;
+            i--;
+            j--;
+        }
+        // 计算完以后的答案需要翻转过来
+        ans.reverse();
+        return ans.toString();
+    }
+
+}
