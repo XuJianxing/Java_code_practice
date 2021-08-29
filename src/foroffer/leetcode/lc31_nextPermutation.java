061bef67ffe7fe871727dfdf760793d60441f4db
XuJianxing <498802124@qq.com>
2021-08-29 23:37:26 +0800

    leetcode 31. 下一个排列

---
+++ src/foroffer/leetcode/lc31_nextPermutation.java
@@ -0,0 +1,46 @@
+package foroffer.leetcode;
+
+/**
+ * 31. 下一个排列
+ * 实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 必须 原地 修改，只允许使用额外常数空间。
+ *
+ * 示例：
+ * 输入：nums = [1,2,3]
+ * 输出：[1,3,2]
+ *
+ * 输入：nums = [3,2,1]
+ * 输出：[1,2,3]
+ */
+public class lc31_nextPermutation {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
