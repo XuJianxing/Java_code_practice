b6318740730e7ee91d722a7c7729ef3fa347b63d
XuJianxing <498802124@qq.com>
2021-09-26 22:12:18 +0800

    offer 7. 重建二叉树

--- src/foroffer/offer/_7_ReConstructBinaryTree.java
+++ src/foroffer/offer/_7_ReConstructBinaryTree.java
@@ -1,11 +1,15 @@
 package foroffer.offer;
 
+import java.util.HashMap;
 
-/*
+/**
+ * 剑指offer 7. 重建二叉树
+ * 给二叉树的前序遍历和中序遍历序列，重建该二叉树返回根节点
+ */
 public class ReConstructBinaryTree {
-    private Map<Integer, Integer> indexForInOrders = new HashMap<>();
+    private HashMap<Integer, Integer> indexForInOrders = new HashMap<>();
 
-    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+    public TreeNode buildTree(int[] pre, int[] in) {
         for (int i = 0; i < in.length; i++)
             indexForInOrders.put(in[i], i);  // 按节点值作Map的key，下标作value存储
         return reConstructBinaryTree(pre, 0, pre.length - 1, 0);
@@ -23,4 +27,3 @@ public class ReConstructBinaryTree {
     }
 
 }
-*/
