1330a0418427f6f5a6c2ffbbba3ac464e64d226e
XuJianxing <498802124@qq.com>
2021-11-07 18:43:43 +0800

    leetcode 128. 最长连续序列

---
+++ src/foroffer/leetcode/lc128_longestConsecutive.java
@@ -0,0 +1,37 @@
+package foroffer.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 128. 最长连续序列
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ * 解法：用哈希表或者集合存储所有数字，然后遍历数组挨个试，判断表里有没有连续递增的数，记录递增可达的最大值
+ */
+public class lc128_longestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
