c591bcafa02d1ece5e9ab9a268b64d7157b07f9d
XuJianxing <498802124@qq.com>
2021-11-07 23:52:23 +0800

    leetcode 300. 最长递增子序列

--- src/foroffer/leetcode/lc300_lengthOfLIS.java
+++ src/foroffer/leetcode/lc300_lengthOfLIS.java
@@ -19,6 +19,25 @@ public class lc300_lengthOfLIS {
      *     遍历直到i结尾
      */
     public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    public int lengthOfLIS_2(int[] nums) {
         if(nums.length == 0) return 0;
         int[] dp = new int[nums.length];
         int res = 0;
@@ -38,7 +57,7 @@ public class lc300_lengthOfLIS {
     /**
      * 优化，二分法降低时间复杂度
      */
-    public int lengthOfLIS_2(int[] nums) {
+    public int lengthOfLIS_0(int[] nums) {
         int[] tails = new int[nums.length];
         int res = 0;
         for(int num : nums) {
