7f38a2156251cf8cd8462217b0044578d581a9ca
XuJianxing <498802124@qq.com>
2021-11-24 23:35:04 +0800

    leetcode 78. 子集

---
+++ src/foroffer/leetcode/lc78_subsets.java
@@ -0,0 +1,36 @@
+package foroffer.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 78. 子集
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ */
+public class lc78_subsets {
+    List<Integer> t = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        // 基于深度优先搜索的回溯法
+        dfs(0, nums);
+        return ans;
+    }
+
+    public void dfs(int cur, int[] nums) {
+        if (cur == nums.length) {
+            ans.add(new ArrayList<>(t));
+            return;
+        }
+        t.add(nums[cur]);
+        dfs(cur + 1, nums);
+        t.remove(t.size() - 1);
+        dfs(cur + 1, nums);
+    }
+
+}
