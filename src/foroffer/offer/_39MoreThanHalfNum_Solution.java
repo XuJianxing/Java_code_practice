656d2e126695bee05962cd0d7687351795ee252a
XuJianxing <498802124@qq.com>
2022-01-13 00:22:12 +0800

    offer 39. 数组中出现次数超过一半的数字

--- src/foroffer/offer/_39MoreThanHalfNum_Solution.java
+++ src/foroffer/offer/_39MoreThanHalfNum_Solution.java
@@ -8,6 +8,21 @@ import java.util.Random;
 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0
  */
 public class _39MoreThanHalfNum_Solution {
+    // 投票法
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = nums[0];
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
     // 用了快排里的partition方法，找第middle位置的数
     public int MoreThanHalfNum_Solution(int [] array) {
         if (array.length <= 0) return 0;
@@ -18,12 +33,11 @@ public class _39MoreThanHalfNum_Solution {
         while (index != middle){
             if (index > middle){
                 end = index - 1;
-                index = _10_5QuickSort.partition(array, start, end);
             }
             else {
                 start = index + 1;
-                index = _10_5QuickSort.partition(array, start, end);
             }
+            index = _10_5QuickSort.partition(array, start, end);
         }
         int result = array[middle];  // 要检查一下，下标为middle位置的数是否真的出现超过一半次数。
         /*
