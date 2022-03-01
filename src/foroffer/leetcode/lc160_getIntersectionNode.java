d1eeb64c0014d42a24f5edb50376a3781648c643
XuJianxing <498802124@qq.com>
2022-03-01 17:29:33 +0800

    leetcode 160. 相交链表

--- src/foroffer/leetcode/lc160_getIntersectionNode.java
+++ src/foroffer/leetcode/lc160_getIntersectionNode.java
@@ -6,10 +6,8 @@ package foroffer.leetcode;
  * 思路1. 链表1存到hashset里，然后遍历链表2看有没有相同节点
  * 思路2. 双指针，两个指针指向AB头结点，然后分别遍历，其中一个指到尾节点之后，重新指到另一个头结点继续遍历，直到另一个也到尾
  */
-public class lc160_getIntersectionNode {
-}
 
-class solution160 {
+public class lc160_getIntersectionNode {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) return null;
         ListNode pA = headA, pB = headB;
