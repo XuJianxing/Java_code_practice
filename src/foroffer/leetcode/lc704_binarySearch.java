69990ac597c0fe59083e296d1261e5f31b7d3b0f
XuJianxing <498802124@qq.com>
2022-03-01 17:28:37 +0800

    leetcode 704. 二分查找

---
+++ src/foroffer/leetcode/lc704_binarySearch.java
@@ -0,0 +1,23 @@
+package foroffer.leetcode;
+
+/**
+ * 704. 二分查找
+ */
+
+public class lc704_binarySearch {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
